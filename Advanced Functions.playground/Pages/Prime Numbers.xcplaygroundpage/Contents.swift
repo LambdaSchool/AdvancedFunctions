be1da3072e99db205c719a14a2bbdf4f0c592f8f
Spencer Curtis <spencercolecurtis@gmail.com>
2019-07-29 09:38:56 -0600

    Add playground with instructions

---
+++ Advanced Functions.playground/Pages/Prime Numbers.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+/*:
+ ## Prime Numbers
+ 
+ Write a function called `printPrimes(upTo n: Int)`. The function should print the prime numbers that are between 1 and `n`. For example, if you pass in 100, the function should print the prime numbers between 1 and 100.
+
+ A number is prime if it is only evenly divisible by itself and 1, and not evenly divisible by any other number. Call your function with several different values for `n` and verify that it prints the correct numbers.
+ 
+ */
+
+/*
+ Rewrite the above function so that it's two functions. One called `printPrimes(upTo n: Int)` and the other called `isPrime(_ x: Int) -> Bool`. `isPrime()` should return true if the passed in number is prime, false otherwise. `printPrimes()` should do the same thing as before, but should call `isPrime()` to check if each number is prime.
+ */
+
+
+//: page 2 of 3  |  [Next: Fibonacci](@next)
---
+++ Advanced Functions.playground/Pages/Return Values.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+/*:
+ ## Return Values
+ 
+ Write a function called `greeting` that takes a `String` argument called name, and returns a `String` that greets the name that was passed into the function. I.e. if you pass in "Dan" the return value might be "Hi, Dan! How are you?" Use the function and print the result.
+ */
+
+
+/*:
+ Write a function that takes two `Int` arguments, and returns an `Int`. The function should multiply the two arguments, then add 2 to that, then return the result.
+
+ Call the function, passing in any two numbers you want for the arguments. Print the returned value from this function.
+ */
+
+
+//: page 1 of 3  |  [Next: Prime Numbers](@next)
+
+//: [Next](@next)
